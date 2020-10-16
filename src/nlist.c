79542abcb10fa0185f7bb71008730ef13e0f395b
Nikola Kolev <koue@chaosophia.net>
2020-10-16 22:10:32 +0000

    Headers clean-up

--- src/nlist.c
+++ src/nlist.c
@@ -28,21 +28,11 @@
  *
  */
 
-#include <ctype.h>
-#include <dirent.h>
 #include <errno.h>
 #include <fts.h>
-#include <sha.h>
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/file.h>
-#include <sys/param.h>
 #include <sys/stat.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <zlib.h>
 
